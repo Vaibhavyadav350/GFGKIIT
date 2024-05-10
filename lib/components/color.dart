7fbd332f3fe8710dc32b824046a0c35d91bbe1e7
Vaibhavyadav350 <vaibhav160404@gmail.com>
2024-05-10 20:21:49 +0530

    Login Updated

---
+++ lib/components/color.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+class ConstantsColor {
+  static const Color kPinkColor = Color(0xFFFE53BB);
+Color vygreen = HexColor("9ADFA0");
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:gfg_kiit/admin/hackathonupload.dart';
-import 'package:gfg_kiit/home/home.dart';
-import 'package:gfg_kiit/user/login.dart';
 import 'package:gfg_kiit/user/wrapper.dart';
-
-import 'admin/uploadmember.dart';
-import 'admin/uploadproject.dart';
-import 'domain/webdev.dart';
-import 'home/homescreen.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -18,7 +8,6 @@ void main() async {
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
